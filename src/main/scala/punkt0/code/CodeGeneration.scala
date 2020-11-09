0193a6d3112c84ec5580fcb5d5b5cd51df9b5d14
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-11-09 20:11:07 +0100

    add lab6 instructions

---
+++ src/main/scala/punkt0/code/CodeGeneration.scala
@@ -0,0 +1,49 @@
+package punkt0
+package code
+
+import ast.Trees._
+import analyzer.Symbols._
+import analyzer.Types._
+import cafebabe._
+import AbstractByteCodes.{New => _, _}
+import ByteCodes._
+
+object CodeGeneration extends Phase[Program, Unit] {
+
+  def run(prog: Program)(ctx: Context): Unit = {
+
+    /** Writes the proper .class file in a given directory. An empty string for dir is equivalent to "./". */
+    def generateClassFile(sourceName: String, ct: ClassDecl, dir: String): Unit = {
+      // TODO: Create code handler, save to files ...
+      ???
+    }
+
+    // a mapping from variable symbols to positions in the local variables
+    // of the stack frame
+    def generateMethodCode(ch: CodeHandler, mt: MethodDecl): Unit = {
+      val methSym = mt.getSymbol
+
+      // TODO: Emit code
+
+      ch.freeze
+    }
+
+    val outDir = ctx.outDir.map(_.getPath+"/").getOrElse("./")
+
+    val f = new java.io.File(outDir)
+    if (!f.exists()) {
+      f.mkdir()
+    }
+
+    val sourceName = ctx.file.get.getName
+
+    // output code
+    prog.classes foreach {
+      ct => generateClassFile(sourceName, ct, outDir)
+    }
+
+    // Now do the main declaration
+    // ...
+  }
+
+}
